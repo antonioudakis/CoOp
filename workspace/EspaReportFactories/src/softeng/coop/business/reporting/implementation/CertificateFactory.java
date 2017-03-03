b25a8058558e2f8fd50ebdf72c49b9557d3ac8f8
grammophone <emmanuel.liossis@gmail.com>
2017-03-03 15:45:21 +0200

    Report type for certification of completion.

---
+++ workspace/EspaReportFactories/src/softeng/coop/business/reporting/implementation/CertificateFactory.java
@@ -0,0 +1,39 @@
+package softeng.coop.business.reporting.implementation;
+
+import java.util.*;
+import softeng.coop.business.reporting.*;
+import org.eclipse.birt.report.engine.api.*;
+
+public class CertificateFactory extends ReportFactoryBase
+{
+	@Override
+	public List<IParameterType> getParametersSpecification()
+	{
+		ArrayList<IParameterType> list = new ArrayList<IParameterType>();
+		
+		list.add(new ParameterType("DIVISION", DataType.String, true));
+
+		return list;
+	}
+
+	protected RenderOption getRenderOption(){
+		RenderOption renderOption = new RenderOption();
+		renderOption.setOption(IRenderOption.HTML_PAGINATION, true);
+		renderOption.setOutputFormat("doc");
+		
+		return renderOption;
+	}
+	
+	@Override
+	protected String getContentType()
+	{
+		return "application/msword";
+	}
+	
+	@Override
+	protected String getFilenameSuffix()
+	{
+		return ".doc";
+	}
+
+}
